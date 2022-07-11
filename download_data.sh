3b5c2a3c860695a30fc4a136c9819c121d8389e8
Kai-46 <kz298@cornell.edu>
2022-07-10 21:19:45 -0400

    release of real data

--- download_data.sh
+++ download_data.sh
@@ -1,6 +1,11 @@
 pip install gdown
 
-gdown 16VnMcF1KJYxN9QId6TClMsZRahHNMW5g
-unzip nerf_llff_data.zip && mv nerf_llff_data llff
+echo "Downloading image indices for Bi et al 2020: Deep Reflectance Volumes: Relightable Reconstructions from Multi-View Photometric Images"
+echo "Please ask the authors of this work for data, and then split the data using the image indices"
+gdown 1BThZgEnHgsL7dgyVTQuSFYZjAkZzQozx
+unzip "Bi et al 2020-image_indices.zip"
 
-
+echo "Downloading real data captured by Luan et al 2021: Unified Shape and SVBRDF Recovery using Differentiable Monte Carlo Rendering"
+echo "Please credit the original paper if you use this data"
+gdown 1BO6XZjUm8PhHof5RZ7O0Y3C815loBlqj
+unzip "Luan et al 2021.zip"
